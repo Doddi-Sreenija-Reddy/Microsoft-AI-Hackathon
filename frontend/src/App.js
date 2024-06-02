8f0ad711900aa46100b130de1a7dc9325cc5dad5
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-06-03 02:51:19 +0530

    route updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home, Landing } from './pages';
+import { Footer, Navbar } from './components';
 
 function App() {
 
   const routes = [
-    { path: '/', component: Landing },
-    { path: '/home', component: Home },
+    { path: '/', element: <Landing/> },
+    { path: '/home', element: <Home/> },
   ];
 
   return (
     <div>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           {routes.map((route, index) => {
             return (
-              <Route key={index} path={route.path} component={route.component} />
+              <Route key={index} path={route.path} element={route.element} exact/>
             );
           })}
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
